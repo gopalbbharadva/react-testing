6da3c5ccca727734dd50e32b3f42f781e6479419
gopalbbharadva <gopalbbharadva@gmail.com>
2023-12-03 13:06:06 +0530

    Feat: grouping the tests using describe method

--- src/components/Greet.test.tsx
+++ src/components/Greet.test.tsx
@@ -1,14 +1,27 @@
 import { render, screen } from '@testing-library/react'
 import { Greet } from './Greet'
 
-test('Greet component', () => {
-  render(<Greet />)
-  const greetElement = screen.getByText(/bye bro/i)
-  expect(greetElement).toBeInTheDocument()
+describe('Greet component test', () => {
+  test('Component', () => {
+    render(<Greet />)
+    const greetElement = screen.getByText(/bye bro/i)
+    expect(greetElement).toBeInTheDocument()
+  })
+
+  // nested
+  // describe('Greet component with prop test', () => {
+  //     test('Component with a person name as a prop', () => {
+  //       render(<Greet name='Gopal' />)
+  //       const greetElement = screen.getByText(/bye Gopal bro/i)
+  //       expect(greetElement).toBeInTheDocument()
+  //     })
+  //   })
 })
 
-test('Greet component with a person name', () => {
-  render(<Greet name='Gopal' />)
-  const greetElement = screen.getByText(/bye Gopal bro/i)
-  expect(greetElement).toBeInTheDocument()
+describe('Greet component with prop test', () => {
+  test('Component with a person name as a prop', () => {
+    render(<Greet name='Gopal' />)
+    const greetElement = screen.getByText(/bye Gopal bro/i)
+    expect(greetElement).toBeInTheDocument()
+  })
 })
